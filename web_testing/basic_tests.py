a15514aafaa5a9293d0f45f5ee3d582db35d3b69
weibik <j-szkola@wp.pl>
2023-10-29 18:54:02 +0100

    corrects test names

--- web_testing/basic_tests.py
+++ web_testing/basic_tests.py
@@ -11,8 +11,13 @@ class MainPageTest(unittest.TestCase):
     def tearDown(self):
         self.web_page.driver.quit()
 
-    def test_web_start(self):
+    def test_main_page(self):
         self.web_page.open_page(TheInternetMainPageLocators.main_url)
-        element = self.web_page.driver.find_element(*TheInternetMainPageLocators.ab_testing)
-        element.click()
+        self.assertEqual(self.web_page.driver.current_url, "https://the-internet.herokuapp.com/")
+        self.assertEqual(self.web_page.driver.title, "The Internet")
+
+    def test_ab_page(self):
+        self.web_page.open_page(TheInternetMainPageLocators.main_url)
+        element_ab_testing = self.web_page.driver.find_element(*TheInternetMainPageLocators.ab_testing_page)
+        element_ab_testing.click()
         self.assertEqual(self.web_page.driver.current_url, "https://the-internet.herokuapp.com/abtest")
--- web_testing/the_internet_elements.py
+++ web_testing/the_internet_elements.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class TheInternetMainPageLocators:
     main_url = "https://the-internet.herokuapp.com/"
-    ab_testing = (By.PARTIAL_LINK_TEXT, "A/B Testing")
+    ab_testing_page = (By.PARTIAL_LINK_TEXT, "A/B Testing")
