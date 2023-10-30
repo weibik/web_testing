32c452ffa14ed043b3c33723a2e76f60c0840f15
weibik <j-szkola@wp.pl>
2023-10-30 18:22:18 +0100

    adds setup and teardown for each test suite

--- web_testing/basic_tests.py
+++ web_testing/basic_tests.py
@@ -1,17 +1,29 @@
 import unittest
-
 from web_testing.basic_page import WebPage
 from web_testing.the_internet_elements import TheInternetMainPageLocators
 from web_testing.the_internet_elements import AddRemovePageLocators
 
 
-class MainPageTests(unittest.TestCase):
-    def setUp(self):
-        self.web_page = WebPage("Chrome")
-        self.web_page.open_page(TheInternetMainPageLocators.main_url)
+class BaseTestClass(unittest.TestCase):
+    web_page = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.web_page = WebPage("Chrome")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.web_page.driver.quit()
+
+
+class MainPageTests(BaseTestClass):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.web_page.open_page(TheInternetMainPageLocators.main_url)
 
     def tearDown(self):
-        self.web_page.driver.quit()
+        self.web_page.open_page(TheInternetMainPageLocators.main_url)
 
     def test_main_page(self):
         self.assertEqual(self.web_page.driver.current_url, "https://the-internet.herokuapp.com/")
@@ -23,16 +35,15 @@ class MainPageTests(unittest.TestCase):
         self.assertEqual(self.web_page.driver.current_url, "https://the-internet.herokuapp.com/abtest")
 
     def test_add_remove_page(self):
-        add_remove_page_hiperlink = self.web_page.driver.find_element(*TheInternetMainPageLocators.add_remove_page)
-        add_remove_page_hiperlink.click()
+        add_remove_page_hyperlink = self.web_page.driver.find_element(*TheInternetMainPageLocators.add_remove_page)
+        add_remove_page_hyperlink.click()
         self.assertEqual(self.web_page.driver.current_url,
                          "https://the-internet.herokuapp.com/add_remove_elements/")
 
 
-class AddRemoveTests(unittest.TestCase):
-    def setUp(self):
-        self.web_page = WebPage("Chrome")
+class AddRemovePageTests(BaseTestClass):
+    def setUpClass(self):
+        super().setUpClass()
         self.web_page.open_page(AddRemovePageLocators.main_url)
 
-    def tearDown(self):
-        self.web_page.driver.quit()
+
--- web_testing/the_internet_elements.py
+++ web_testing/the_internet_elements.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.by import By
 class TheInternetMainPageLocators:
     main_url = "https://the-internet.herokuapp.com/"
     ab_testing_page = (By.PARTIAL_LINK_TEXT, "A/B Testing")
+    add_remove_page = (By.PARTIAL_LINK_TEXT, "Add/Remove Elements")
+
+class AddRemovePageLocators:
+    main_url = "https://the-internet.herokuapp.com/add_remove_elements/"
