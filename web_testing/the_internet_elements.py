914547d69bd6aeb92c1dc9687fde252b7929bc41
weibik <j-szkola@wp.pl>
2023-11-01 14:14:34 +0100

    adds func to check if element exist and adds delete button locator

--- web_testing/the_internet_elements.py
+++ web_testing/the_internet_elements.py
@@ -6,6 +6,8 @@ class TheInternetMainPageLocators:
     ab_testing_page = (By.PARTIAL_LINK_TEXT, "A/B Testing")
     add_remove_page = (By.PARTIAL_LINK_TEXT, "Add/Remove Elements")
 
+
 class AddRemovePageLocators:
     main_url = "https://the-internet.herokuapp.com/add_remove_elements/"
     add_element_button = (By.XPATH, "//button[contains(text(),'Add Element')]")
+    delete_button = (By.XPATH, "//button[contains(text(),'Delete')]")
