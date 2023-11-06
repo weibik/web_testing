4efd75c85fd115fc50d1eb2b46c3dfefc9c5584b
weibik <j-szkola@wp.pl>
2023-11-06 22:51:29 +0100

    extends script a bit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
 import unittest
 
-from web_testing.basic_tests import AddRemovePageTests, MainPageTests
+from web_testing.basic_tests import AddRemovePageTests, MainPageTests, BasicAuthTests, BrokenImageTests
 
 # Define the test suite
 test_suite = unittest.TestSuite()
 
 # Add your test cases to the suite based on the command-line arguments
-parser = argparse.ArgumentParser(description='Run specific tests.')
+parser = argparse.ArgumentParser(description='Run web tests with specific suite.')
 parser.add_argument('-basic', action='store_true', help='Run basic tests')
 parser.add_argument('-add_remove', action='store_true', help='Run AddRemovePageTests')
+parser.add_argument('-basic_auth', action='store_true', help='Run BasicAuthTests')
+parser.add_argument('-broken_img', action='store_true', help='Run BrokenImageTests')
 
 args = parser.parse_args()
 
@@ -18,6 +20,12 @@ if args.basic:
 
 if args.add_remove:
     test_suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(AddRemovePageTests))
+
+if args.basic_auth:
+    test_suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(BasicAuthTests))
+
+if args.broken_img:
+    test_suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(BrokenImageTests))
 
 # Run the test suite
 unittest.TextTestRunner().run(test_suite)
