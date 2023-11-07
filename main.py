18f2edd208e9e9ec94b56f56661be534c90d4474
weibik <j-szkola@wp.pl>
2023-11-07 21:28:10 +0100

    transforms main script to pytest

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import argparse
-import unittest
 
-from web_testing.basic_tests import AddRemovePageTests, MainPageTests, BasicAuthTests, BrokenImageTests
+import pytest
 
-test_suite = unittest.TestSuite()
 test_classes = {
-    'basic': MainPageTests,
-    'add_remove': AddRemovePageTests,
-    'basic_auth': BasicAuthTests,
-    'broken_img': BrokenImageTests
+    'basic': "basic_auth",
+    'add_remove': "add_remove_page",
+    'basic_auth': "basic_auth",
+    'broken_img': "broken_img"
 }
 
 parser = argparse.ArgumentParser(description='Run web tests with specific suite.')
 for test_name in test_classes.keys():
     parser.add_argument(f'-{test_name}', action='store_true', help=f'Run {test_name.replace("_", " ").title()} Tests')
+parser.add_argument('-n', '--processes', type=int, default=4, help='Number of parallel processes')
 args = parser.parse_args()
 
+pytest_args = []
 for test_name, test_class in test_classes.items():
     if getattr(args, test_name):
-        test_suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(test_class))
+        pytest_args.append(f"tests/test_{test_classes[test_name]}.py")
 
-# Run the test suite
-unittest.TextTestRunner().run(test_suite)
+pytest.main(['-n', str(args.processes)] + pytest_args)
+# poetry run python main.py -add_remove -n 3
