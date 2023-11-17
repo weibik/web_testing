803a7312682de3f832ad7467349c46b0e696a26e
weibik <j-szkola@wp.pl>
2023-11-17 19:37:33 +0100

    extends basic tests

--- web_testing/the_internet_locators.py
+++ web_testing/the_internet_locators.py
@@ -5,6 +5,7 @@ class TheInternetMainPageLocators:
     main_url = "https://the-internet.herokuapp.com/"
     ab_testing_page = (By.PARTIAL_LINK_TEXT, "A/B Testing")
     add_remove_page = (By.PARTIAL_LINK_TEXT, "Add/Remove Elements")
+    basic_auth_page = (By.PARTIAL_LINK_TEXT, "Basic Auth")
 
 
 class AddRemovePageLocators:
