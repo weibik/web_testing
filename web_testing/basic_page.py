d9901db7687018003f1bbbeda5e680afe3794ae3
weibik <j-szkola@wp.pl>
2023-11-26 16:42:31 +0100

    fixes required types

--- web_testing/basic_page.py
+++ web_testing/basic_page.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 class WebPage(Webdriver):
-    def open_page(self, url):
+    def open_page(self, url: str):
         self.driver.get(url)
 
     def get_element(self, *args):
@@ -15,32 +15,32 @@ class WebPage(Webdriver):
     def get_elements(self, *args):
         return self.driver.find_elements(*args)
 
-    def get_element_by_id(self, element_id):
+    def get_element_by_id(self, element_id: str):
         return self.driver.find_element(By.ID, element_id)
 
-    def get_element_by_link_text(self, link_text):
+    def get_element_by_link_text(self, link_text: str):
         return self.driver.find_element(By.LINK_TEXT, link_text)
 
-    def get_element_by_partial_link_text(self, partial_link_text):
+    def get_element_by_partial_link_text(self, partial_link_text: str):
         return self.driver.find_element(By.PARTIAL_LINK_TEXT, partial_link_text)
 
-    def get_element_by_xpath(self, xpath):
+    def get_element_by_xpath(self, xpath: str):
         return self.driver.find_element(By.XPATH, xpath)
 
-    def get_element_by_css_selector(self, css_selector):
+    def get_element_by_css_selector(self, css_selector: str):
         return self.driver.find_element(By.CSS_SELECTOR, css_selector)
 
-    def get_elements_by_tag_name(self, tag_name):
+    def get_elements_by_tag_name(self, tag_name: str):
         return self.driver.find_elements(By.TAG_NAME, tag_name)
 
-    def check_if_exists(self, locator_type, locator):
+    def check_if_exists(self, locator_type: By, locator: str):
         try:
             self.driver.find_element(locator_type, locator)
         except NoSuchElementException:
             return False
         return True
 
-    def check_if_visible(self, locator_type, locator, timeout=10):
+    def check_if_visible(self, locator_type: str, locator: str, timeout=10):
         try:
             element_present = EC.visibility_of_element_located((locator_type, locator))
             WebDriverWait(self.driver, timeout).until(element_present)
