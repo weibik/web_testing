61f07d23632b34eb5612160f790dcb2fdbc0ddb1
weibik <j-szkola@wp.pl>
2023-11-26 16:39:44 +0100

    Revert "adds required arguments types"
    This reverts commit 91a697c48de2d928dd7fe67fb5d791feb9e34cb8.

--- web_testing/basic_page.py
+++ web_testing/basic_page.py
@@ -15,47 +15,46 @@ class WebPage(Webdriver):
     def get_elements(self, *args):
         return self.driver.find_elements(*args)
 
-    def get_element_by_id(self, element_id: str):
+    def get_element_by_id(self, element_id):
         return self.driver.find_element(By.ID, element_id)
 
-    def get_element_by_link_text(self, link_text: str):
+    def get_element_by_link_text(self, link_text):
         return self.driver.find_element(By.LINK_TEXT, link_text)
 
-    def get_element_by_partial_link_text(self, partial_link_text: str):
+    def get_element_by_partial_link_text(self, partial_link_text):
         return self.driver.find_element(By.PARTIAL_LINK_TEXT, partial_link_text)
 
-    def get_element_by_xpath(self, xpath: str):
+    def get_element_by_xpath(self, xpath):
         return self.driver.find_element(By.XPATH, xpath)
 
-    def get_element_by_css_selector(self, css_selector: str):
+    def get_element_by_css_selector(self, css_selector):
         return self.driver.find_element(By.CSS_SELECTOR, css_selector)
 
-    def get_elements_by_tag_name(self, tag_name: str):
+    def get_elements_by_tag_name(self, tag_name):
         return self.driver.find_elements(By.TAG_NAME, tag_name)
 
-    def check_if_exists(self, locator: tuple):
+    def check_if_exists(self, locator_type, locator):
         try:
-            self.driver.find_element(locator)
+            self.driver.find_element(locator_type, locator)
         except NoSuchElementException:
             return False
         return True
 
-    def check_if_visible(self, locator: tuple, timeout: int = 10):
+    def check_if_visible(self, locator_type, locator, timeout=10):
         try:
-            element_present = EC.visibility_of_element_located(locator)
+            element_present = EC.visibility_of_element_located((locator_type, locator))
             WebDriverWait(self.driver, timeout).until(element_present)
         except (TimeoutException, NoSuchElementException):
             return False
         return True
 
-    def refresh(self, count: int = 1):
-        for i in range(count):
-            self.driver.refresh()
+    def refresh(self):
+        self.driver.refresh()
 
     def wait(self, amount_of_time: int = 5):
         self.driver.implicitly_wait(amount_of_time)
 
-    def wait_for_visibility(self, locator: tuple, timeout:int = 3):
-        WebDriverWait(self.driver, timeout).until(
-            EC.presence_of_element_located(locator)
+    def wait_for_visibility(self, locator):
+        WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, locator))
         )
