e685ace9e407780cf7514954fb5ec3fbf171f569
weibik <j-szkola@wp.pl>
2023-12-03 14:53:28 +0100

    add jquery ui tests

---
+++ tests/test_jquery_ui.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+import pytest
+import requests
+from selenium.webdriver import Keys, ActionChains
+
+from web_testing.basic_page import WebPage
+from web_testing.the_internet_locators import JQueryMenuLocators
+
+
+@pytest.fixture(scope="function")
+def web_page(request):
+    web_page = WebPage("Chrome")
+    web_page.open_page(JQueryMenuLocators.main_url)
+    yield web_page
+    web_page.driver.quit()
+
+
+def test_jquery_ui_page(web_page):
+    response = requests.get(JQueryMenuLocators.main_url)
+    assert response.status_code == 200
+
+
+def test_jquery_menu(web_page):
+    enabled = web_page.get_element(*JQueryMenuLocators.enabled)
+    actions = ActionChains(web_page.driver)
+    actions.move_to_element(enabled).perform()
+    time.sleep(1)
+    downloads = web_page.get_element(*JQueryMenuLocators.downloads)
+    assert web_page.driver.execute_script("return $(arguments[0]).is(':visible');", downloads)
+    actions.move_to_element(downloads).perform()
+    time.sleep(1)
+    pdf = web_page.get_element(*JQueryMenuLocators.pdf)
+    assert web_page.driver.execute_script("return $(arguments[0]).is(':visible');", pdf)
+
