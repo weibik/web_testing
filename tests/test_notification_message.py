2c8af42904a70382129f101daf050398aca5c856
weibik <j-szkola@wp.pl>
2023-12-07 20:50:24 +0100

    fixes notification msg

--- tests/test_notification_message.py
+++ tests/test_notification_message.py
@@ -1,31 +1,29 @@
-import logging
-
 import pytest
 import requests
 
 from web_testing.basic_page import WebPage
-from web_testing.the_internet_locators import NotificationMessage
+from web_testing.the_internet_locators import NotificationMessageLocators
 
 
 @pytest.fixture(scope="function")
 def web_page():
     web_page = WebPage("Chrome")
-    web_page.open_page(NotificationMessage.main_url)
+    web_page.open_page(NotificationMessageLocators.main_url)
     yield web_page
     web_page.driver.quit()
 
 
 def test_notification_message_page(web_page):
-    response = requests.get(NotificationMessage.main_url)
+    response = requests.get(NotificationMessageLocators.main_url)
     assert response.status_code == 200
 
 
 def test_notification_message(web_page):
     num_of_tests = 3
     for i in range(num_of_tests):
-        web_page.get_element(*NotificationMessage.reload_message_button).click()
-        web_page.wait_for_visibility(*NotificationMessage.message)
-        message = web_page.get_element(*NotificationMessage.message)
+        web_page.get_element(*NotificationMessageLocators.reload_message_button).click()
+        web_page.wait_for_visibility(*NotificationMessageLocators.message)
+        message = web_page.get_element(*NotificationMessageLocators.message)
         message_list = ["Action unsuccesful, please try again", "Action successful"]
         print(f"Message {i} -> {message.text}\n")
         is_valid_msg = any(item in message.text for item in message_list)
